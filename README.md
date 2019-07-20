07c4e55e7e8144f1b6fcb7b35cb97cab7e3a2d61
menahishayan <menahi.shayan@gmail.com>
2019-07-21 01:18:28 +0530

    v2.1 commit

--- README.md
+++ README.md
@@ -1,2 +1,71 @@
 # HomeScript
-Python script for command line control of HomeBridge (HomeKit)
+Python script for command line control of [HomeBridge](https://github.com/nfarina/homebridge) (HomeKit)
+
+Used to toggle Homebridge accessories On or Off via python script, no Home app required. You can set up a cronjob to enable automation, scripting and mimic other HomeKit functionalities
+
+## Examples
+`python homeScript.py MainLight 0`  | Switch off
+`python homeScript.py MainLight 1`  | Switch off
+`python homeScript.py MainLight`   | Toggle
+
+## Easy Match
+The script doesn't require full names of the accessories.
+For example, if your light is called "MainLight", you can run:
+`python homeScript.py MainLight 0`
+or
+`python homeScript.py main 0`
+The script will automatically search for matching substrings and toggle the accessory
+
+## Dependencies
+ - Python requests library
+ 
+ ## Installation
+  - Install the requests library `pip install requests` or `pip3 install requests`
+  - Move homeScript.py to a convenient location
+  - Edit the script to include your homebridge URL, port and authorization key if necessary.
+  - Permissions `chmod +x /path/to/homeScript.py`
+
+## Usage
+`python homeScript.py [option] [value]`
+### Options:
+ - list : lists all available HomeKit accessories
+   - Usage: `python homeScript.py list [argument]`
+   - Arguments:
+     - <none> : lists accessory names
+     - aid : lists accessory names with AID value
+     - iid : lists accessory names with IID value
+     - id : lists accessory names with AID and IID
+     - type : lists accessory names with type [Lightbulb, Switch, Fan, etc.]
+     - value : lists accessory names current state
+ - <accessory-name> : toggles the accessory On or Off, or sets to value
+   - Usage: `python homeScript.py <accessory-name> [value]`
+ - help : prints usage info
+
+## PRs and Commit Template
+PRs and commits that you make to this repo must include the following:
+Type: bug-fix or enhancement
+Description: Brief description about what the commit achieves
+Notes: (heads ups/pointers to other developers if necessary)
+
+<hr/>
+
+## Changelog
+### v2.1
+- Added type support to identify accessory type
+- Updated help doc
+
+### v2.0
+- List of accessories are now automatically fetched from the homebridge, instead of having to manually set them up in the script
+
+### v1.3
+- Added easy name matching
+
+### v1.2
+- Added help doc
+- Added listing
+
+### v1.1
+- Added toggling
+
+### v1.0
+- Initial release
---
+++ homeScript.py
@@ -0,0 +1,97 @@
+#!/usr/bin/python
+
+# HomeScript - Python script to control homebridge devices using the command line
+# v2.1
+# Created by Menahi Shayan. 2019.
+
+import requests
+import sys
+
+url = 'http://home.local:35945/'
+headers = {'Content-Type': 'Application/json','authorization': '031-45-154',}
+
+accessories={}
+
+def getAccessories():
+    getAcc = requests.get(url + 'accessories', headers=headers)
+    for item in getAcc.json()['accessories']:
+#        accessories.append({'aid':item['aid'],'iid':item['services'][1]['characteristics'][1]['iid'],'name':item['services'][1]['characteristics'][0]['value'],'value':item['services'][1]['characteristics'][1]['value']})
+        accessories.update({str(item['services'][1]['characteristics'][0]['value']) : {'aid':item['aid'],'iid':item['services'][1]['characteristics'][1]['iid'],'type':item['services'][0]['characteristics'][2]['value'],'value':item['services'][1]['characteristics'][1]['value']}})
+    return accessories
+
+def selectAccessory(inputName):
+    global accessoryName
+    for key in accessories:
+        if inputName in key.lower():
+            accessoryName = key
+            return accessories[key]
+    return {'aid':-1, 'iid':-1, 'type':'null', 'value': -1}
+
+def printAccessories(param=''):
+    for key in accessories:
+        if param == 'aid':
+            print str(accessories[key]['aid']) + ' ',
+        if param == 'iid':
+            print str(accessories[key]['iid']) + ' ',
+        if param == 'id' or param == 'all':
+            print str(accessories[key]['aid']) + '.' + str(accessories[key]['iid']) + ' ',
+        print key,
+        if param == 'type' or param == 'all':
+            print str(accessories[key]['type']) + ' ',
+        if param == 'value' or param == 'all':
+            print str(accessories[key]['value']),
+        print ''
+
+def printHelp():
+    print 'Usage: python homeScript.py [option] [value]'
+    print 'Options:'
+    print '\n\tlist : lists all available HomeKit accessories'
+    print '\t\tUsage: python homeScript.py list [argument]'
+    print '\t\tArguments:'
+    print '\t\t\t<none> : lists accessory names'
+    print '\t\t\taid : lists accessory names with AID value'
+    print '\t\t\tiid : lists accessory names with IID value'
+    print '\t\t\tid : lists accessory names with AID and IID'
+    print '\t\t\ttype : lists accessory names with type'
+    print '\t\t\tvalue : lists accessory names current state'
+    print '\n\t<accessory-name> : toggles the accessory On or Off, or sets to value'
+    print '\t\t\tUsage: python homeScript.py <accessory-name> [value]'
+    print '\n\thelp : prints usage info'
+    print '\nEg: python homeScript.py MainLight'
+    print '    python homeScript.py main 0'
+    print '\nCreated by Menahi Shayan.\n'
+    sys.exit()
+
+if len(sys.argv)==1:
+    printHelp()
+
+if sys.argv[1] == 'help':
+    printHelp()
+
+getAccessories()
+
+if sys.argv[1] == 'list':
+    printAccessories(sys.argv[2] if len(sys.argv)>2 else '')
+    sys.exit()
+
+obj = selectAccessory(sys.argv[1].lower())
+
+if obj['aid'] == -1:
+    print 'Accessory not found.\nHere are a list of accessories:\n'
+    printAccessories()
+    print '\nFor usage info type \'python homeScript.py help\''
+    sys.exit()
+
+if len(sys.argv)>2:
+    setVal = str(sys.argv[2])
+elif obj['value'] == True:
+    setVal = '0'
+else:
+    setVal = '1'
+
+setReq = requests.put(url + 'characteristics', headers=headers, data='{"characteristics":[{"aid":' + str(obj['aid']) + ',"iid":' + str(obj['iid']) + ',"value":' + setVal + '}]}')
+
+if setReq.json()['characteristics'][0]['status'] == 0:
+    print accessoryName + ' is ' + ('On' if setVal == '1' else 'Off')
+else:
+    print 'An error occurred'
