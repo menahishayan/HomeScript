e9910367ac264bd22cca0df2e109c419acfc3023
MenahiShayan <shayan1232001@yahoo.co.in>
2021-03-03 01:10:07 +0530

    pip deploy prep

--- README.md
+++ README.md
@@ -8,72 +8,59 @@ Used to toggle Homebridge accessories On or Off via python script, no Home app r
 **Note:** as of HomeScript 4.1 the minimum requirement is Python3+ and HomeBridge 1.x.x+
 
 ## Examples
-`homeScript.py -s MainLight 0`  | Switch off  
-`homeScript.py -s MainLight 1`  | Switch on  
-`homeScript.py -s MainLight`   | Toggle  
-`homeScript.py -s lifx --hue 140` | RGB Lights Support  
-`homeScript.py -s lifx --saturation` | Toggle even works on integer values
+`hs.py -s MainLight 0`  | Switch off  
+`hs.py -s MainLight 1`  | Switch on  
+`hs.py -s MainLight`   | Toggle  
+`hs.py -s lifx --hue 140` | RGB Lights Support  (brightness, hue, saturation, temperature)
 
 ## Easy Match
 The script doesn't require full names of the accessories.  
 
 **For example**, if your light is called "MainLight", you can run:  
-`homeScript.py -s MainLight 0` or  
-`homeScript.py -s main 0`  
+`hs.py -s MainLight 0` or  
+`hs.py -s main 0`  
 The script will automatically search for matching substrings and set the accessory value
 
 ## Group Actions
 You can set multiple accessories (of the same type) in a single command:  
-`homeScript.py -s all lights 1`  
-`homeScript.py -s all switches 0`  
-
-## Automations
-HomeScript allows you to create automations **without** a Home Hub!  
-Read: [AUTOMATION.md](AUTOMATION.md)
-
-## HS Apps
-Introducing HomeScript Apps! Allows for modularity and extendability using custom third-party apps and scripts. WIP
+`hs.py -s all lights 1`  
+`hs.py -s all switches 0`  
 
 **Looking for maintainers: This repo is no longer being maintained and future updates to this project may not be guaranteed. If you are interested in maintaining this project, feel free to reach out.** 
 
 ## Dependencies
- - Python requests library
- - Python JSON library
+ - Python HomeScript API
 
 ## Setup
   Important: HomeBridge **must** be run in insecure mode for this script to work!
 
-### Running HomeBridge in Insecure Mode
-#### via Command Line
-Add the `-I` flag:
-
+#### Running HomeBridge in Insecure Mode
+ - via Command Line
 ```
 homebridge -I
 ```
 
-#### via Systemd
-
+ - via Systemd
 ```
 HOMEBRIDGE_OPTS=-U /var/lib/homebridge -I
 ```
 
-#### via pm2
-
+ - via pm2
 ```
 pm2 stop homebridge; pm2 delete homebridge; pm2 start homebridge -- -I
 ```
 
 ## Installation
-  - Install the requests library `pip install requests` or `pip3 install requests`
-  - On your client computer: Move **homeScript.py** to a convenient location
+  - Install the HomeScript API `pip install homescript` or `pip3 install homescript`
+  - On your client computer: Download **hs.py** and move it to a convenient location
     - Edit the script to include your homebridge URL, port and authorization key
-    - Change permissions `chmod +x /path/to/homeScript.py` (on linux)
+    - Change permissions `chmod +x /path/to/hs.py` (on linux)
 
 ## Usage
-Usage: `homeScript.py [option] [value]`
+Usage: `hs.py [option] [value]`
 ### Options:
  - -l, --list    : Lists all available HomeKit accessories
-  - Usage: `homeScript.py -l [argument]`
+  - Usage: `hs.py -l [argument]`
   - Arguments:
     - \<none\> : lists accessory names
     - aid : lists accessory names with AID value
@@ -82,31 +69,59 @@ Usage: `homeScript.py [option] [value]`
     - type : lists accessory names with type
     - value : lists accessory names current state
  - -g, --get     :  [EasyMatch] gets current value of accessory
-   - Usage: `homeScript.py -g <accessory-name>`
+   - Usage: `hs.py -g <accessory-name>`
  - -s, --set     :  [EasyMatch] toggles the accessory On or Off, or sets to the specified value
-   - Usage: `homeScript.py -s <accessory-name> [value]`
+   - Usage: `hs.py -s <accessory-name> [value]`
    - Arguments:
      - \<accessory-name\> : accessory that you want to change
-     - -b, --brightness : adjusts accessory brightness
-     - -h, --hue : adjusts accessory hue
-     - -sat, --saturation : adjusts accessory saturation
-     - -t, --temperature : adjusts accessory color temperature
+     - -b : adjusts accessory brightness
+     - -hue : adjusts accessory hue
+     - -sat : adjusts accessory saturation
+     - -t : adjusts accessory color temperature
      - [value] : value that you want to set it to
  - all     :  Gets or sets value of multiple HomeKit accessories
-   - Usage: `homeScript.py -g all <accessory-type>`
-homeScript.py -s all <accessory-type> value
+   - Usage: `hs.py -g all <accessory-type>`
+hs.py -s all <accessory-type> value
  - -d, --debug   : generates debug log file.
-   - Usage: `homeScript.py -d <command>`
-   - Eg: `homeScript.py -d -s all lights 0`
+   - Usage: `hs.py -d <command>`
+   - Eg: `hs.py -d -s all lights 0`
  - -h, --help    : prints usage info
  - -v, --version : prints HomeScript version
 
+## API
+As of v5.1+ HomeScript is now a fully importable API
+```python
+import HomeScript
+
+# Initialize with hostname, port and auth code. Debug and sys.argv are optional
+hs = HomeScript(hostname, port, auth, [debug], [sys.argv])
+
+# Select an accessory or group of accessories. Any get/set/print operation requires accessories to be selected first.
+hs.selectAccessory('mainlight')
+hs.selectGroup('lights')
+
+hs.printSelectedItems()
+
+# Operates on all selected items
+hs.setStates(1)
+hs.setValues('Brightness',250)
+```
+| Function | Description |
+| getAccessories() | Returns raw list of all available accessories on the Bridge |
+| selectAccessories( searchString: str ) | Saves matching accessory into selectedAccessories and returns them |
+| selectGroup( searchString: str ) | Saves all matching accessories into selectedAccessories and returns them |
+| printAccessories() | Prints all available accessories on the Bridge |
+| printSelectedItems() | Prints selectedAccessories |
+| getSelectedItems() | Returns selectedAccessories |
+| setStates(state: Boolean) | Sets `state` as the on/off value of all item(s) in selectedAccessories. If no argument is specified it toggles the state of all selectedAccessories |
+| setValues(attribute: str, value: int) | Sets numeric `value` to attribute of the item(s) in selectedAccessories. Attrubute: Brightness/Hue/Saturation/Color Temperature |
+| getVersion() | Does what it says on the can... |
 
 ## Troubleshooting/Error Reporting/Contributing
 The `debug` option helps generate a logfile for troubleshooting and error detection.  
  - If you face an error, open a new issue on this repo prefixed by [Error] describing the error and attach your **both** your debug log and your exception log, along with any other outputs you receive.
  - If you would like to help improve the tool or request features, open an issue prefixed by [Feature Request] describing the functionality.
- - You **must** attach your debug log or else your issue will be closed. A simple debug log can be obtained from `homeScript.py -d -l`
+ - You **must** attach your debug log or else your issue will be closed. A simple debug log can be obtained from `hs.py -d -l`
 
 ## PRs and Commit Template
 PRs and commits that you make to this repo must include the following:  
@@ -125,6 +140,10 @@ PRs and commits that you make to this repo must include the following:
 <hr/>
 
 ## Changelog
+### v5.1
+ - Rewritten from the ground up to be object oriented
+ - Now features an importable API
+
 ### v5.0
 - Support for RGB lights! Now lets you control brightness, hue, saturation and color temperature of your lights!
 - Added additional value format recognition
---
+++ homescript/__main__.py
@@ -0,0 +1,205 @@
+# HomeScript - Python script to control homebridge devices using the command line
+# v5.1
+# Created by Menahi Shayan. 2019.
+# https://github.com/menahishayan/HomeScript
+
+import requests
+import json
+import logging
+from datetime import date
+# to add webcolors
+
+class HomeScript:
+	def __init__(self,hostname,port, auth, debug=None, argv=None):
+		self.__version__ = '5.1.0'
+		self.url = 'http://' + hostname + ':' + str(port) + '/'
+		self.headers = {'Content-Type': 'Application/json', 'authorization': auth}
+
+		self.accessories={}
+		self.selectedAccessories=[]
+		self.selectedAccessoryNames={}
+
+		self.getAccessories()
+
+		self.debug = debug
+		self.exceptionFile='homescript_exception_' + date.today().strftime("%Y.%m.%d") + '.log'
+		if self.debug:
+			self.debugHandler('init',argv)
+
+	def getAccessories(self):
+		global getAcc
+		try:
+			getAcc = requests.get(self.url + 'accessories', headers=self.headers)
+			if getAcc.status_code == 200:
+				getAcc = getAcc.json()
+			# load sample data for debugging
+			# with open('acc.json') as f:
+			#   data = json.load(f)
+			for item in getAcc['accessories']:
+				if getAcc['accessories'].index(item) != 0:
+					interfaces = []
+					for i in item['services'][1]['characteristics'][1:]:
+						if i['format'] not in ['bool','string','tlv8','uint8','float']:
+							interfaces.append({'iid':i['iid'],'description': i['description'],'maxValue': i['maxValue'],'minValue': i['minValue'],'minStep': i['minStep'], 'value': i['value']})
+						else:
+							interfaces.append({'iid':i['iid'],'description': i['description'],'value': i['value']})
+					self.accessories.update({str(item['services'][0]['characteristics'][3]['value']).replace(' ','_') : {'aid':item['aid'],'iid':10,'type':item['services'][0]['characteristics'][2]['value'],'value':interfaces}})
+
+		except:
+			if self.debug:
+				logging.error(Exception, exc_info=True)
+			print('Exception logged: ' + self.exceptionFile)
+		return self.accessories
+
+	def selectAccessory(self,inputName):
+		try:
+			for key in self.accessories:
+				if inputName in key.lower():
+					self.selectedAccessoryNames.update({self.accessories[key]['aid']:{'name':key}})
+					self.selectedAccessories.append({'aid':self.accessories[key]['aid'], 'value':self.accessories[key]['value']})
+			return self.selectedAccessories
+		except:
+			if self.debug:
+				logging.error(Exception, exc_info=True)
+				print('Exception logged: ' + self.exceptionFile)
+
+	def selectGroup(self,inputName):
+		try:
+			for key in self.accessories:
+				if self.accessories[key]['type'].lower().startswith(inputName[:len(inputName)-2]):
+					self.selectedAccessoryNames.update({self.accessories[key]['aid']:{'name':key}})
+					self.selectedAccessories.append({'aid':self.accessories[key]['aid'], 'iid':self.accessories[key]['iid'], 'value':self.accessories[key]['value']})
+			return self.selectedAccessories
+		except:
+			if self.debug:
+				logging.error(Exception, exc_info=True)
+				print('Exception logged: ' + self.exceptionFile)
+
+	def printAccessories(self,param=''):
+		try:
+			if param == 'json':
+				print(json.dumps(self.accessories))
+				return
+			for key in self.accessories:
+				if param == 'aid':
+					print(str(self.accessories[key]['aid']) + ' ', end='')
+				print(key, end=' ')
+				if param == 'type' or param == 'all':
+					print(str(self.accessories[key]['type']) + ' ', end='')
+				if param in ['value','iid','all']:
+					# print(str(self.accessories[key]['value']), end='')
+					for i in self.accessories[key]['value']:
+						print('\n   ', end='')
+						if param == 'iid' or param == 'all':
+							print(str(i['iid']) + ' ' + str(i['value']), end=' ')
+						if param == 'value' or param == 'all':
+							print(str(i['description']) + ' ' + str(i['value']), end='')
+				print('')
+		except:
+			if self.debug:
+				# debugHandler(json.dumps(sys.exc_info()))
+				logging.error(Exception, exc_info=True)
+				print('Exception logged: ' + self.exceptionFile)
+
+	def getVersion(self):
+		return self.__version__
+
+	def getSelectedItems(self):
+		return self.selectedAccessories
+
+	def printSelectedItems(self):
+		for item in self.selectedAccessories:
+			print(self.selectedAccessoryNames[item['aid']]['name'] + ' ' + str(item['value']))
+
+	def setStates(self,value):
+		setData = []
+		valueIndex = 0
+
+		for item in self.selectedAccessories:
+			if value.isdigit():
+				item['value'][0]['value'] = int(value)
+			elif item['value'][0]['value'] == 0 or item['value'][0]['value'] == False:
+				item['value'][0]['value'] = 1
+			else:
+				item['value'][0]['value'] = 0
+
+			self.selectedAccessoryNames[item['aid']].update({'iid': item['value'][valueIndex]['iid'], 'value': item['value'][valueIndex]['value']})
+			setData.append({'aid': item['aid'],'iid': item['value'][valueIndex]['iid'], 'value': item['value'][valueIndex]['value']})
+		setReq = requests.put(self.url + 'characteristics', headers=self.headers, data='{"characteristics":' + str(setData).replace('\'','\"') + '}')
+
+		if self.debug:
+			self.debugHandler('Set characteristics response:\n'+setReq.text)
+			self.debugHandler('End set characteristics response')
+
+		try:
+			if setReq.status_code != 204:
+				print(setReq)
+				for item in setReq.json()['characteristics']:
+					print(self.selectedAccessoryNames[item['aid']]['name'] + ' Error: ' + str(item['status']))
+					return item['status']
+		except:
+			if self.debug:
+				logging.error(Exception, exc_info=True)
+				print('Exception logged: ' + self.exceptionFile)
+
+	def setValues(self, attribute, value):
+		setData = []
+		valueIndex = 0
+
+		for item in self.selectedAccessories:
+			valueIndex = next((item['value'].index(v) for v in item['value'] if v['description'] == attribute), 0)
+
+			if value.isdigit():
+				if (int(value) <= item['value'][valueIndex]['maxValue']) and (int(value) >= item['value'][valueIndex]['minValue']) and (int(value)%item['value'][valueIndex]['minStep'] == 0):
+					item['value'][valueIndex]['value'] = int(value)
+				else:
+					print('Error:\n   Max Value: ' + str(item['value'][valueIndex]['maxValue']) + '\n   Min Value: ' + str(item['value'][valueIndex]['minValue']) +  '\n   Min Step: ' + str(item['value'][valueIndex]['minStep']))
+			elif item['value'][valueIndex]['value'] >= ((item['value'][valueIndex]['maxValue']-item['value'][valueIndex]['minValue'])/2) - (((item['value'][valueIndex]['maxValue']-item['value'][valueIndex]['minValue'])/2)%item['value'][valueIndex]['minStep']):
+				item['value'][valueIndex]['value'] = item['value'][valueIndex]['maxValue']
+			else:
+				item['value'][valueIndex]['value'] = item['value'][valueIndex]['minValue']
+
+			self.selectedAccessoryNames[item['aid']].update({'iid': item['value'][valueIndex]['iid'], 'value': item['value'][valueIndex]['value']})
+			setData.append({'aid': item['aid'],'iid': item['value'][valueIndex]['iid'], 'value': item['value'][valueIndex]['value']})
+		setReq = requests.put(self.url + 'characteristics', headers=self.headers, data='{"characteristics":' + str(setData).replace('\'','\"') + '}')
+
+		if self.debug:
+			self.debugHandler('Set characteristics response:\n'+setReq.text)
+			self.debugHandler('End set characteristics response')
+
+		try:
+			if setReq.status_code != 204:
+				print(setReq)
+				for item in setReq.json()['characteristics']:
+					print(self.selectedAccessoryNames[item['aid']]['name'] + ' Error: ' + str(item['status']))
+					return item['status']
+		except:
+			if self.debug:
+				logging.error(Exception, exc_info=True)
+				print('Exception logged: ' + self.exceptionFile)
+
+	def debugHandler(self,content='init',argv=''):
+		if content=='init':
+			logging.basicConfig(filename=self.exceptionFile,filemode = 'a',encoding='utf-8', level=logging.DEBUG)
+			debugFile=open('homescript_debug_' + date.today().strftime("%Y.%m.%d") + '.log', "w")
+			debugFile.write('HSDB: homescript_debug_' + date.today().strftime("%Y.%m.%d") + '.log\nHSDB: HomeScript version: ' + self.__version__)
+			debugFile.write('\nHSDB: URL: ' + self.url + '\nHSDB: Headers: ' + json.dumps(self.headers))
+			debugFile.write('\nHSDB: Accessories: ' + json.dumps(self.accessories))
+			debugFile.write('\nHSDB: Selected accessories: ' + json.dumps(self.selectedAccessories))
+			debugFile.write('\nHSDB: Selected accessory names: ' + json.dumps(self.selectedAccessoryNames))
+			debugFile.write('\nHSDB: Arguments: ' + json.dumps(argv))
+			debugFile.write('\nHSDB: Get accessories response:\n' + str(getAcc) + '\nHSDB: End get accessories response\n')
+		elif content=='end':
+			debugFile=open('homescript_debug_' + date.today().strftime("%Y.%m.%d") + '.log', "a")
+			debugFile.write('HSDB: End homeScript debug file')
+			print('Debug logged: homescript_debug_' + date.today().strftime("%Y.%m.%d") + '.log')
+		else:
+			debugFile=open('homescript_debug_' + date.today().strftime("%Y.%m.%d") + '.log', "a")
+			debugFile.write('HSDB: ' + str(content) + '\n')
+		debugFile.close()
+
+	def __del__(self):
+		try:
+			self.debugHandler('end')
+		except:
+			None
