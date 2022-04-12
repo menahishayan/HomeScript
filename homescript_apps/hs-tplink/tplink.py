3d6014264294fd82bc9e741113e93e35a9526baf
Pedro Costa <nikelect@hotmail.com>
2022-04-12 22:11:30 +0100

    Add TP-LINK HS110 library, to get the relay current state. Add example HS110. Fix Blink Led Service path.

---
+++ homescript_apps/hs-tplink/tplink.py
@@ -0,0 +1,23 @@
+""" Reads the temperature and humidity from a Xiaomi Aqara Temperature and Humidity Sensor
+    through a Aqara Air Conditioning Companion Gateway.
+"""
+
+def get_state(hs):
+    """ Reads the state of the realy from a TP-Link Smart Plug. 
+
+    Parameters
+    ----------
+    hs : HomeScript
+         HomeBridge Connection.
+    """
+    hs_print_output = hs.getSelectedItems()
+    #print(hs_print_output)
+    for item in hs_print_output:
+        #print(item)
+        #print(item["value"])
+        #print(item["value"][0])
+        #print(item["value"][0]["description"])
+        if item["value"][0]["description"] == 'On':
+            state = item["value"][0]["value"]
+            return state 
+    print("Smart Plug Sensor Not Found!")
