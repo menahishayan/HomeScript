6111ac836cdd99185053abd66b3337063d7030fb
Shayan <menahi.shayan@gmail.com>
2023-01-01 20:37:51 +0530

    Update homescript_apps/hs-xiaomi/xiaomi.py

--- homescript_apps/hs-xiaomi/xiaomi.py
+++ homescript_apps/hs-xiaomi/xiaomi.py
@@ -1,10 +1,6 @@
 def get_temperature(hs):
     hs_print_output = hs.getSelectedItems()
     for item in hs_print_output:
-        #print(item)
-        #print(item["value"])
-        #print(item["value"][0])
-        #print(item["value"][0]["description"])
         if item["value"][0]["description"] == 'Current Temperature':
             temperature = item["value"][0]["value"]
             return temperature # °C
