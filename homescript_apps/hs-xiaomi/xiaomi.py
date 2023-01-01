e870baa1268253d187fc15e5d8ed8eff50de65af
Shayan <menahi.shayan@gmail.com>
2023-01-01 20:38:04 +0530

    Update homescript_apps/hs-xiaomi/xiaomi.py

--- homescript_apps/hs-xiaomi/xiaomi.py
+++ homescript_apps/hs-xiaomi/xiaomi.py
@@ -9,10 +9,6 @@ def get_temperature(hs):
 def get_humidity(hs):
     hs_print_output = hs.getSelectedItems()
     for item in hs_print_output:
-        #print(item)
-        #print(item["value"])
-        #print(item["value"][0])
-        #print(item["value"][0]["description"])
         if item["value"][0]["description"] == 'Current Relative Humidity':
             humidity = item["value"][0]["value"]
             return humidity # %
